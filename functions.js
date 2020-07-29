d61d1e4fc88170209c50ac5f13f62ad27a310f1b
vagrant <vagrant@vagrant.vm>
2020-07-29 01:40:35 +0000

    functions updated including tests cases

--- functions.js
+++ functions.js
@@ -75,3 +75,42 @@ function getFullName (firstName, lastName){
 
 //     return subtotal + fee
 // Define your function here
+
+function calculateTotal(basePrice, state, tax = 0.05) {
+
+  const subTotal = basePrice * (1 + tax);
+  
+// subTotal's formula is the same always so const
+
+  let fee = 0;
+  
+// let as fee will vary depending on case
+
+  if (state === 'CA') {
+
+// === triple equality better in JavaScript for absolute equality
+
+// comparisons must be in () in Javascript
+
+    fee = 0.03 * subTotal;
+
+// else if rather than elif in JavaScript
+
+    } else if (state === 'PA') {
+      fee = 2;
+    } else if (state === 'MA') {
+      if (basePrice <=100) {
+        fee = 1;
+    } else {
+        fee = 3;
+    }
+  }
+
+  return subTotal + fee;
+}
+
+//Our Test Cases:
+//calculateTotal(100,'CA') // Expect 100 + 5 + 3.15 = 108.15 
+//calculateTotal(100,'PA') // Expect 100 + 5 + 2 = 107
+//calculateTotal(103,'MA') // Expect 103 + 5.15 + 3 = 111.15 
+//calculateTotal(100,'MA') // Expect 100 + 5 + 1 = 106
